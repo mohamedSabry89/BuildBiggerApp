bc88ededbca2f16eb4a3b908ca376cdb7b49a549
Mohamed Sabry Basha <m-sabry89@hotmail.com>
2020-11-07 21:08:43 +0300

    5 commit

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,9 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.example.displayjokes.DisplayJokes;
 import android.example.jokesprovider.JokesProvider;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -44,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
         Intent intent = new Intent(this, DisplayJokes.class);
         JokesProvider jokesProvider = new JokesProvider();
         String theJoke = jokesProvider.tellingJokes();
